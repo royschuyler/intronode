5d3c719cf18d69d4464619cee375d5011284a7eb
royschuyler <royschuyler@yahoo.com>
2015-07-21 15:35:36 -0500

    getting name from prompt

---
+++ cheer.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var prompt = require('prompt');
+prompt.start();
+
+prompt.get('name', function (err, result) {
+    printHelloMessage(result.name)
+});
+
+function printHelloMessage(name) {
+  var lowerCase = name.toLowerCase();
+  var nameArr = lowerCase.split('');
+  for (i = 0; i < nameArr.length; i++) {
+    var prefix;
+    if (nameArr[i] === 'a' || nameArr[i] === 'e' || nameArr[i] === 'i' || nameArr[i] === 'o' || nameArr[i] === 'u'){
+      prefix = 'an';
+    }
+    else{
+      prefix = 'a '
+    }
+    var string = 'Give me ' + prefix + ' ' + nameArr[i]
+    console.log(string)
+   }
+  }
